1331d0104217b88f7e2cdda631ad020be31a45fe
fatemebaghi <79264335+fatemebaghi@users.noreply.github.com>
2021-04-15 22:04:38 +0430

    Create equation2D.py

---
+++ equation2D.py
@@ -0,0 +1,16 @@
+def Delta(a,b,c):
+    return ((b**2)-(4*a*c))
+
+def equation2D(a,b,c):
+    if Delta(a,b,c) < 0 :
+        return print("This function has no answer")
+    elif Delta(a,b,c) == 0 :
+        x = -b/(2*a)
+        return print("This function has an answer. The answer is {}.".format(x))
+    elif Delta(a,b,c) > 0 :
+        x1 = (-b +(Delta(a,b,c)**2))/(a*2)
+        x2 = (-b -(Delta(a,b,c)**2))/(a*2)
+        return print("This function has two answers. The answers are {} and {}.".format(x1,x2))
+
+    
+
