5ae4e5304486fc31520e8abddf736260aa217723
fatemebaghi <79264335+fatemebaghi@users.noreply.github.com>
2021-04-16 17:00:14 +0430

    Update sawp.py

--- sawp.py
+++ sawp.py
@@ -1,7 +1,9 @@
-def sawp(a,b):
-    c = a
-    a = b
-    b = c
-    return (a,b)
-
+def sawp (a,b):
+	return(a,b)
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=a
+a=b
+b=c
+print(a,b)
 
