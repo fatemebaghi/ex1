7e0ecdb4c6e97df72b0c88557c94d7046a4a11d9
fatemebaghi <79264335+fatemebaghi@users.noreply.github.com>
2021-04-16 17:04:34 +0430

    Delete Rand2.py

--- Rand2.py
+++
@@ -1,9 +0,0 @@
-import random
-def Rand2(a,b):
-    x=random.randint(a,b)
-    if x % 2==0:
-        return x
-    elif x % 2 !=0 and a<=x<b:
-        return x+1
-    elif x % 2 !=0 and x==b:
-        return x-1
